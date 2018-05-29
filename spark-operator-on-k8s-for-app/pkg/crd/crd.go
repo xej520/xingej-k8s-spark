054aff87431d1aefec974b4f2a056e232cf3dcfd
邢尔军 <xingej@163.com>
2018-05-29 18:42:59 +0800

    注册CRD模块：增加校验逻辑，校验是否已经注册过CRD类型

--- spark-operator-on-k8s-for-app/pkg/crd/crd.go
+++ spark-operator-on-k8s-for-app/pkg/crd/crd.go
@@ -3,16 +3,27 @@ package crd
 import (
 	"xingej-go/xingej-k8s-spark/spark-operator-on-k8s-for-app/pkg/crd/sparkapplication"
 	"xingej-go/xingej-k8s-spark/spark-operator-on-k8s-for-app/pkg/util"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"fmt"
 )
 
 // 注册SparkApplication类型的CRD
 func RegisterCRDForSparkApplication(endpoint, configPath string) {
-	// 1、创建SparkApplication对应的CRD对象
-	sparkAppCrdObject := sparkapplication.GetCRDObject()
-
-	// 2、创建extension 客户端
+	// 1、创建extension 客户端
 	apiExtensionClient := util.KubeExtensionClient(endpoint, configPath)
 
-	// 3、开始注册到k8s
+	// 2、创建SparkApplication对应的CRD对象
+	sparkAppCrdObject := sparkapplication.GetCRDObject()
+
+	//3、校验是否已经注册过CRD
+	if existing, err := apiExtensionClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get(sparkAppCrdObject.Name, v1.GetOptions{});
+		err == nil && existing != nil {
+		// 说明已经注册过了
+		// 就不再进行重复注册，当前处理逻辑：退出即可
+		fmt.Printf("已经注册过 %s CRD了", sparkAppCrdObject.Name)
+		return
+	}
+
+	// 4、开始注册到k8s
 	util.RegisterCRD(apiExtensionClient, sparkAppCrdObject)
 }
